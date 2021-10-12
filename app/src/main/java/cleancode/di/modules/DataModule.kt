627d274c4bd93f9593052520beffe416fd9c3e35
Nygar <nygar.91@gmail.com>
2021-10-12 08:27:24 +0200

    Fixed Hilt DI issues

--- app/src/main/java/cleancode/di/modules/DataModule.kt
+++ app/src/main/java/cleancode/di/modules/DataModule.kt
@@ -16,16 +16,14 @@ import cleancode.repository.DataRepositoryImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
-import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
 
 @Module
-@InstallIn(SingletonComponent::class)
+@InstallIn(ViewModelComponent::class)
 class DataModule {
 
     @Provides
-    @Singleton
     fun provideDataRepository(@ApplicationContext app: Context,
                               api: RestApi,
                               categoryCache: CategoryCache,
@@ -37,25 +35,21 @@ class DataModule {
 
     //Database
     @Provides
-    @Singleton
     fun provideCategoryCache(@ApplicationContext app: Context,
                              database: AppDatabase
     ): CategoryCache = CategoryCacheImpl(app, database)
 
     @Provides
-    @Singleton
     fun provideMessageCache(@ApplicationContext  app: Context,
                             database: AppDatabase
     ): MessageCache = MessageCacheImpl(app, database)
 
     @Provides
-    @Singleton
     fun provideuserCache(@ApplicationContext  app: Context,
                          database: AppDatabase
     ): UserCache = UserCacheImpl(app, database)
 
     @Provides
-    @Singleton
     fun provideUserLoggedCache(@ApplicationContext  app: Context,
                                database: AppDatabase
     ): UserLoggedCache = UserLoggedCaheImpl(app, database)

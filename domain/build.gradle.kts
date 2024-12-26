534b831589d41016401e605c7aeb558d40760e74
Nygar <nygar.91@gmail.com>
2024-12-26 05:52:23 +0100

    feat(Gradle): Config for packages

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,10 +1,61 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     id("cleanarchitecturekotlin.android.library")
     alias(libs.plugins.ksp)
+    `maven-publish`
 }
 
 android {
     namespace = "com.nygar.domain"
+}
+
+publishing {
+
+    /**Create github.properties in root project folder file with gpr.usr=GITHUB_USER_ID  & gpr.key=PERSONAL_ACCESS_TOKEN**/
+    val githubProperties: Properties =
+        Properties().apply {
+            load(FileInputStream(File(rootProject.rootDir, "github.properties")))
+        }
+
+    fun getVersionName(): String {
+        return "0.0.1" // Replace with version Name
+    }
+
+    fun getArtificatId(): String {
+        return "cleanarchitecturekotlinlib" // Replace with library name ID
+    }
+
+    publications {
+        create<MavenPublication>("gpr") {
+            run {
+                groupId = "com.nygar"
+                artifactId = getArtificatId()
+                version = getVersionName()
+                artifact(layout.buildDirectory.dir("/outputs/aar/domain-debug.aar").get())
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            /** Configure path of your package repository on Github
+             *  Replace GITHUB_USERID with your/organisation Github userID and REPOSITORY with the repository name on GitHub
+             */
+            url = uri("https://maven.pkg.github.com/Nygar/CleanArchitectureKotlin")
+            credentials {
+                /**Create github.properties in root project folder file with gpr.usr=GITHUB_USER_ID  & gpr.key=PERSONAL_ACCESS_TOKEN
+                 * OR
+                 * Set environment variables
+                 */
+                username = githubProperties["gpr.usr"] as String? ?: System.getenv("GPR_USER")
+                password = githubProperties["gpr.key"] as String? ?: System.getenv("GPR_API_KEY")
+
+            }
+        }
+    }
 }
 
 dependencies {
